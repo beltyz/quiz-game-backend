36825c0e31244b555f67e44d3173502dbcc211fe
beltyz <Lushchyk321@gmail.com>
2026-01-11 16:48:10 +0100

    add nullable for getuserinfo

--- QuizGame/QuizGame.Application/Interfaces/IUserService.cs
+++ QuizGame/QuizGame.Application/Interfaces/IUserService.cs
@@ -4,7 +4,7 @@ namespace QuizGame.Application.Interfaces;
 
 public interface IUserService
 {
-    Task<UserDTO> GetUserInfo(string UserId);
+    Task<UserDTO?> GetUserInfo(string UserId);
     Task<bool> UpdateUserInfo(UserDTO user, string UserId);
     Task<bool> DeleteUser(string UserId);
     Task<List<ShortQuizInfoDTO>> GetAllUsersQuiz(string username);
